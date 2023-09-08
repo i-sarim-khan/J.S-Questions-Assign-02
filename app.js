2c7c18a80e78f89bebd0b8b2366a5cab850912bc
i-sarim-khan <136439929+i-sarim-khan@users.noreply.github.com>
2023-09-08 15:20:24 +0500

    JS assignment from book question solution

---
+++ app.js
@@ -0,0 +1,171 @@
+//! Q NO 1 //
+
+// TODO var addition = 3 + 5;
+// ?document.write("Sum of 3 and 5 is" + addition)
+
+//! Q NO 2 //
+
+// TODO var subration = 3-5;
+//? document.write("Subration of 3 and 5" is + "     " +"  "+ subration)
+
+// ! Q NO 2 a //
+ 
+//TODO var mutliplication = 3*5;
+//? document.write("Mutliplication of 3 and 5 is" + " "+ mutliplication)
+
+// ! Q NO 2 b //
+// TODO var division = 3/5;
+// * var text = "Division of 3 and 5 is "
+// ? document.write(text + " " + division);
+
+// ! Q NO 3 a & b //
+
+//  TODO var value ;
+//? document.write('Value after variable decleration is ' + value);
+
+// ! Q NO 3 c & d //
+
+// TODO var value = 5;
+// ? document.write("Initial Value" + " " + value);
+
+
+// ! Q NO 3 e  //
+
+
+ // TODO var x = 5;
+ // TODO var y = ++x;
+// ? document.write("Value after increment is" + " " + value);
+
+
+// ! Q NO 3 f //
+
+
+// TODO var x = 5;
+// TODO var y = ++x;
+// TODO var z = y +7;
+// ? document.write("Value after addition" + " "+z)
+
+
+// ! Q NO 3 g & h //
+
+
+// TODO var x = 5;
+// TODO var y = ++x;
+// TODO var z = y +7;
+// ? document.write("Value after addition" + " "+z)
+
+// ! Q NO 3 i & j //
+
+// TODO var x = 5;
+// TODO var y = ++x;
+// TODO var z = y +7;
+// TODO var p = --z;
+// ? document.write("Value after  decrement " + " "+p)
+
+
+// ! Q NO 3 K & l //
+
+//TODO var x = 5;
+//TODO  var y = ++x;
+//TODO var z = y +7;
+// TODO var p = --z; 
+//TODO  var q = p % 3 ;
+// ? document.write("The reminder is " + " "+q)
+
+
+// ! Q NO 4 // 
+
+// TODO var oneTicket = 600;
+// TODO var fiveTicket = oneTicket * 5 ;
+// TODO var result = fiveTicket;
+// ? document.write('Total cost to buy 5 tickets to a movie is '+ result +"PKR")
+
+// ! Q NO 5 //
+
+// TODO var x = 4;
+
+// TODO for (i=1 ; i<=10; i++){
+//  TODO   let y = `${x} X ${i} = ${x*i} <br/>`
+//  ?   document.write(y)
+// * }
+
+
+// ! Q NO 6 a & b // 
+// TODO  var C= 25;
+// TODO  var cTof= (25*9/5)+32
+// ? document.write( C + '&deg;C'+ "  " + "is" + " "+cTof +'F&deg;') ;
+
+// ! Q NO c & d //
+
+// TODO var F = 70;
+// TODO var fToc= (70-32)*5/9;
+// ? document.write( F + 'F&deg;' +  " " + "is"+" "+ fToc +'&deg;C');
+
+
+
+// ! Q NO 7 //
+
+//TODO  var priceItemOne = 650;
+// ? document.write("Price of item 1 is "+priceItemOne +"<br/>") ;
+// TODO var quantityItemOne = 3;
+// ? document.write("Quantity of item 1 is "+quantityItemOne + "<br/>" )
+
+//TODO  var priceItemTwo = 100;
+// ? document.write("Price of item 2 is "+priceItemTwo +"<br/>") ;
+// TODO var quantityItemTwo = 7;
+//?  document.write("Quantity of item 2 is "+quantityItemTwo + "<br/>" )
+//TODO  var shoppingCharge = 100;
+//?  document.write("Shopping charges "+shoppingCharge + "<br/>" )
+
+//TODO  var total = priceItemOne * quantityItemOne + priceItemTwo * quantityItemTwo + shoppingCharge;
+// ? document.write("Total cost of your order is "+total)
+
+
+
+// ! Q NO 8 // 
+
+//TODO var totalMarks = 980;
+// ? document.write("Total marks :" + totalMarks +"<br/>")
+//TODO var obtMarks = 804;
+// ? document.write("Marks obtained : " + obtMarks +"<br/>")
+//TODO var percentage = 804/980*100;
+// ? document.write("Percentage :" + percentage)
+
+
+// ! Q NO 9 //
+
+// TODO var usDoller =10;
+//TODO var riyadcur = 25;
+// TODO var inPKR = usDoller * 307.79 + riyadcur * 82.05305;
+//? document.write("Total Currency in PKR :" + inPKR)
+
+// ! Q NO 10 //
+
+// TODO var x = 5;
+//TODO var y = x+ 5*10/2;
+// ? document.write('Result is ' + y)
+
+// ! Q NO 11 //
+
+//TODO var current = 2016;
+//TODO var birth = 1992;
+//TODO var age = 2016-1992;
+// ? document.write("Your Age is:"+ age);
+
+// ! Q NO 12 // 
+
+//TODO  var radius = 20;
+//?  document.write("Radius of a circle:" + radius +"<br/>");
+// TODO var circum = 2 *3.142*20;
+// ? document.write("The circumference is :" + circum +"<br/>");
+// TODO var aera = 3.142*20*20;
+// ? document.write("The aera is :"+aera);
+
+// ! Q NO 13 //
+
+// var snack = "chocolate chip ";
+// var currentAge = 19;
+// var EstMaxAge = 65;
+// var AmountSnack = 3;
+// var totalSnack = 41*12*30 *3;
+// document.write(`You will need ${totalSnack} chocolate chip to last you until the ripe old age of  ${EstMaxAge} `)
